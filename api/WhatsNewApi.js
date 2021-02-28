c3d1de0320f2d6b852ba19a8724d3d5b908f1864
Tashila <starbuks1298@gmail.com>
2021-02-28 21:31:26 +0530

    Added Whats new api function

---
+++ api/WhatsNewApi.js
@@ -0,0 +1,11 @@
+import BposApiAxiosInstance from './instances/BposAPI';
+
+// Get Whats New Data
+export const GetWhatsNewData = () => {
+  const config = {
+    url: '/whats-new-articles',
+    method: 'get',
+  };
+
+  return BposApiAxiosInstance(config);
+};

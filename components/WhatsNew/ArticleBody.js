1efc4583e812e49d9f0fd6ec89bc2a9a6bffb389
Tashila <starbuks1298@gmail.com>
2021-03-01 12:42:02 +0530

    Added Markdown to jsx package and rendered article body

--- components/WhatsNew/ArticleBody.js
+++ components/WhatsNew/ArticleBody.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Link from 'next/link';
 import propTypes from 'prop-types';
+import Markdown from 'markdown-to-jsx';
 import Image from 'next/image';
 
 import {articleDateFormat} from '../../helpers/dateHelper';
@@ -12,7 +12,7 @@ const ArticleBody = ({article}) => {
         <div className='article-image'>
           <Image
             width='730'
-            height='500'
+            height='520'
             src={`${process.env.API_URL}${article?.bannerImage?.url}`}
             alt={`Image-${article?.articleName}`}
           />
@@ -31,65 +31,9 @@ const ArticleBody = ({article}) => {
               </li>
             </ul>
           </div>
-
-          <h3>{article?.articleName}</h3>
-
-          <p>
-            Quuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quia non numquam eius modi tempora incidunt ut
-            labore et dolore magnam dolor sit, consectetur.
-          </p>
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-            in reprehenderit in sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat.
-          </p>
-
-          <blockquote className='flaticon-quote'>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus aliquid praesentium eveniet illum asperiores, quidem,
-              ipsum voluptatum numquam ducimus nisi exercitationem dolorum facilis Repellendus aliquid praesentium eveniet illum asperiores.
-            </p>
-          </blockquote>
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-            in reprehenderit in sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat. Lorem ipsum
-            dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat.
-          </p>
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-            in reprehenderit in sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat.
-          </p>
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-            in reprehenderit in sed quia non numquam.
-          </p>
-        </div>
-
-        <div className='post-navigation'>
-          <div className='navigation-links'>
-            <div className='nav-previous'>
-              <Link href='#'>
-                <a>
-                  <i className='bx bx-left-arrow-alt'></i> Prev Post
-                </a>
-              </Link>
-            </div>
-            <div className='nav-next'>
-              <Link href='#'>
-                <a>
-                  Next Post <i className='bx bx-right-arrow-alt'></i>
-                </a>
-              </Link>
-            </div>
+          {/* <h3>{article?.articleName}</h3> */}
+          <div className='pt-5'>
+            <Markdown>{article?.articleBody}</Markdown>
           </div>
         </div>
       </div>

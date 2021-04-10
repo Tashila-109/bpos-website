c9bfc7830c7ec54962ceb79d7c935377aa56756c
Tashila <starbuks1298@gmail.com>
2021-04-10 07:43:12 +0530

    Added in context files

---
+++ context/services/servicesState.js
@@ -0,0 +1,3 @@
+import React, {useReducer} from 'react';
+import ServicesContext from './servicesContext';
+import {SET_SERVICE_LINKS} from '../types';
---
+++ context/types.js
@@ -0,0 +1,1 @@
+export const SET_SERVICE_LINKS = 'SET_SERVICE_LINKS';

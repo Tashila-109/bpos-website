aea8de2b3cf43141f29f658461a8d2df6199a319
Tashila <starbuks1298@gmail.com>
2021-04-14 22:39:09 +0530

    Added success page and fixed form

--- components/HomeOne/MainBanner.js
+++ components/HomeOne/MainBanner.js
@@ -3,57 +3,57 @@ import Link from 'next/link';
 import ReactWOW from 'react-wow';
 
 const MainBanner = () => {
-    return (
-        <div className="main-banner-area">
-			<div className="container-fluid">
-				<div className="row align-items-center">
-					<div className="col-lg-6">
-						<div className="banner-text">
-							<ReactWOW animation='fadeInUp' delay='0.1s'>
-								<span>Investment Retirement Insurance</span>
-							</ReactWOW>
+  return (
+    <div className='main-banner-area'>
+      <div className='container-fluid'>
+        <div className='row align-items-center'>
+          <div className='col-lg-6'>
+            <div className='banner-text'>
+              <ReactWOW animation='fadeInUp' delay='0.1s'>
+                <span>Thank you for contacting us</span>
+              </ReactWOW>
 
-							<ReactWOW animation='fadeInUp' delay='0.2s'>
-								<h1>The Right Protection to Keep You Moving Forward</h1>
-							</ReactWOW>
+              <ReactWOW animation='fadeInUp' delay='0.2s'>
+                <h1>Your message was successfully submitted.</h1>
+              </ReactWOW>
 
-							<ReactWOW animation='fadeInUp' delay='0.4s'>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum.</p>
-							</ReactWOW>
+              <ReactWOW animation='fadeInUp' delay='0.4s'>
+                <p>We will get back to you shortly. Click below to go to the Home Page or check our Services.</p>
+              </ReactWOW>
 
-							<ReactWOW animation='fadeInUp' delay='0.6s'>
-								<div className="banner-btn">
-									<Link href="#">
-										<a className="default-btn">Get Started</a>
-									</Link>
+              <ReactWOW animation='fadeInUp' delay='0.6s'>
+                <div className='banner-btn'>
+                  <Link href='/'>
+                    <a className='default-btn'>Home</a>
+                  </Link>
 
-									<Link href="#">
-										<a className="default-btn active">Find An Agent</a>
-									</Link>
-								</div>
-							</ReactWOW>
-						</div>
-					</div>
+                  <Link href='/services'>
+                    <a className='default-btn active'>Services</a>
+                  </Link>
+                </div>
+              </ReactWOW>
+            </div>
+          </div>
 
-					<div className="col-lg-6 pr-0">
-						<ReactWOW animation='fadeInUp' delay='0.2s'>
-							<div className="banner-img">
-								<img src="/images/banner/banner-img.png" alt="Image" />
-							</div>
-						</ReactWOW>
-					</div>
-				</div>
-			</div>
+          <div className='col-lg-6 pr-0'>
+            <ReactWOW animation='fadeInUp' delay='0.2s'>
+              <div className='banner-img'>
+                <img src='/images/logo.png' alt='Image' />
+              </div>
+            </ReactWOW>
+          </div>
+        </div>
+      </div>
 
-            {/* Shape Images */}
-			<div className="shape">
-				<img src="/images/banner/banner-shape.png" alt="Image" />
-			</div>
-			<div className="banner-shape-right">
-				<img src="/images/banner/banner-shape-right.png" alt="Image" />
-			</div>
-		</div>
-    )
-}
+      {/* Shape Images */}
+      <div className='shape'>
+        <img src='/images/banner/banner-shape.png' alt='Image' />
+      </div>
+      <div className='banner-shape-right'>
+        <img src='/images/banner/banner-shape-right.png' alt='Image' />
+      </div>
+    </div>
+  );
+};
 
 export default MainBanner;

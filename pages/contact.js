d6f7ca5425ffbcff1bcbc9991515f8cbbb5daf77
Tashila <starbuks1298@gmail.com>
2021-04-14 10:26:11 +0530

    Added in cms integration for contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,25 +1,39 @@
 import React from 'react';
+import propTypes from 'prop-types';
+
 import NavbarTwo from '../components/_App/NavbarTwo';
 import PageBanner from '../components/Common/PageBanner';
 import ContactForm from '../components/Contact/ContactForm';
 import Map from '../components/Contact/Map';
 import Footer from '../components/_App/Footer';
 
-const Contact = () => {
-    return (
-        <React.Fragment>
-            <NavbarTwo />
-            <PageBanner 
-                pageTitle="Contact" 
-                homePageUrl="/" 
-                homePageText="Home" 
-                activePageText="Contact" 
-            /> 
-            <ContactForm />
-            <Map />
-            <Footer />
-        </React.Fragment>
-    )
-}
+import {GetContactPageData} from '../api/PagesApi';
+
+const Contact = ({data}) => {
+  return (
+    <React.Fragment>
+      <NavbarTwo />
+      <PageBanner pageTitle='Contact' homePageUrl='/' homePageText='Home' activePageText='Contact' />
+      <ContactForm contactData={data} />
+      <Map />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+Contact.propTypes = {
+  data: propTypes.object.isRequired,
+};
+
+export const getStaticProps = async () => {
+  const {data} = await GetContactPageData();
+
+  return {
+    props: {
+      data,
+    },
+    revalidate: 1,
+  };
+};
 
 export default Contact;
